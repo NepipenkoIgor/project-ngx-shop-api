69178bf7596b028b26b311a2917e13ee577d9de4
Vladislav Kritskiy <vlad@Developers-iMac.local>
2020-04-08 12:33:52 +0300

    Feature: add validation for feedback rate [ref:no-ref]

--- src/feedbacks/dtos/feedbacks.dto.ts
+++ src/feedbacks/dtos/feedbacks.dto.ts
@@ -1,8 +1,11 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Max, Min } from 'class-validator';
 
 export class FeedbackDto {
   @ApiProperty()
   public readonly product!: string;
+  @Min(0)
+  @Max(5)
   @ApiProperty()
   public readonly rate!: number;
   @ApiPropertyOptional()
